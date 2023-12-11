42de96cff1ef927f3b70b6af0e70fc2d76bfa6da
yashrajput0508 <yasharajput0508@gmail.com>
2023-12-11 18:33:06 +0530

    commit

--- src/components/routes/router.js
+++ src/components/routes/router.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"
 import mainrouter from "./mainrouter";
 
-const router = createBrowserRouter([ mainrouter]);
+const router = createBrowserRouter([ mainrouter],{basename: '/HungryHub'});
 
 export {router};
